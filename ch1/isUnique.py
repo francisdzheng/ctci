5f75867c18b8f1eba80074dd2f812431e47fb0f1
Francis Zheng <francisdzheng@berkeley.edu>
2018-01-19 01:03:33 -0800

    1.1

--- ch1/isUnique.py
+++ ch1/isUnique.py
@@ -2,4 +2,16 @@ def unique(str):
     if len(str) > 128:
        return False
     
-    
+    char_set = [False for _ in range(128)]
+
+    for c in string:
+        val = ord(c)
+
+        if char_set[val]:
+            return False
+        
+        char_set[val] = True
+
+    return True
+
+#Runs in O(n) time, where n is len(str)
