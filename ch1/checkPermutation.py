73f417e9644eeb7fb518343295fd0f08b145e70f
Francis Zheng <francisdzheng@berkeley.edu>
2018-01-19 01:16:15 -0800

    1.2

--- ch1/checkPermutation.py
+++ ch1/checkPermutation.py
@@ -1,2 +1,36 @@
+import unittest
+
 def checkPermutation(str1, str2):
+    if len(str1) != len(str2):
+        return False
     
+    s1 = ''.join(sorted(str1))
+    s2 = ''.join(sorted(str2))
+
+    return s1 == s2
+
+class Test(unittest.TestCase):
+    dataT = (
+        ('abcd', 'bacd'),
+        ('3563476', '7334566'),
+        ('wef34f', 'wffe34'),
+    )
+    dataF = (
+        ('abcd', 'd2cba'),
+        ('2354', '1234'),
+        ('dcw4f', 'dcw5f'),
+    )
+
+    def test_cp(self):
+        # true check
+        for test_strings in self.dataT:
+            result = checkPermutation(*test_strings)
+            self.assertTrue(result)
+        # false check
+        for test_strings in self.dataF:
+            result = checkPermutation(*test_strings)
+            self.assertFalse(result)
+
+
+if __name__ == "__main__":
+    unittest.main()
